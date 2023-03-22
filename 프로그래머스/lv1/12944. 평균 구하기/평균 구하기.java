f2b52e1253b511940c0a7a7ee838443f6a8ab3fd
jodonghyeon3 <117457834+jodonghyeon3@users.noreply.github.com>
2023-03-22 23:49:58 +0900

    [level 1] Title: 평균 구하기, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for (int item : arr) {
+            answer += item;
+        }
+        return answer / arr.length;
+    }
+}
