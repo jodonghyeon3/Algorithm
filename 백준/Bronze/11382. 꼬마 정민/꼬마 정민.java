fae65277fdd5d76db5975095f1d4378e803ddcda
jodonghyeon3 <117457834+jodonghyeon3@users.noreply.github.com>
2023-03-30 21:29:52 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        long fir = sc.nextLong();
+        long sec = sc.nextLong();
+        long thi = sc.nextLong();
+
+        System.out.println(fir + sec + thi);
+    }
+}
