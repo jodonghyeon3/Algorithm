ce4c2e7556a8c0ab7f936505c1db4dd131c6dde6
jodonghyeon3 <117457834+jodonghyeon3@users.noreply.github.com>
2023-04-10 23:19:47 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] quiz) {
+       String[] answer = new String[quiz.length];
+
+        for (int i = 0; i < quiz.length; i++) {
+            if (!isCollect(quiz[i])) {
+                answer[i] = "X";
+            } else {
+                answer[i] = "O";
+            }
+
+        }
+        return answer;
+    }
+    public static boolean isCollect(String s) {
+         String[] sArr = s.split(" ");
+        if (sArr[1].equals("-")) {
+            return Integer.valueOf(sArr[0]) - Integer.valueOf(sArr[2]) == Integer.valueOf(sArr[4]) ? true : false;
+        } else {
+            return Integer.valueOf(sArr[0]) + Integer.valueOf(sArr[2]) == Integer.valueOf(sArr[4]) ? true : false;
+        }
+
+
+    }
+}
