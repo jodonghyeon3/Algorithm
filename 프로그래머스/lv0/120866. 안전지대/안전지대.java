421ba2c53f79bdfe3288352d86b1931a5477f429
jodonghyeon3 <117457834+jodonghyeon3@users.noreply.github.com>
2023-04-11 22:24:53 +0900

    [level 0] Title: 안전지대, Time: 0.06 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int solution(int[][] board) {
+        int[][] boardDP = new int[board.length + 1][board[0].length + 1];
+        // if (board.length < 3 || board[0].length < 3) {
+        //     return 0;
+        // }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 1) {
+                    isBomb(boardDP, i, j);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if(boardDP[i][j] == 0) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    public void isBomb(int[][] boardDP, int x, int y) {
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1; j++) {
+                if (i >= 0 && j >= 0 && i < boardDP.length && j < boardDP[0].length) {
+                    boardDP[i][j] = 1;
+                }
+            }
+        }
+    }
+}
