21019a7c9871a74409b0501e0232d141904fc2db
jodonghyeon3 <117457834+jodonghyeon3@users.noreply.github.com>
2023-08-06 22:16:41 +0900

    [level 1] Title: 신규 아이디 추천, Time: 2.47 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,32 +1,6 @@
 class Solution {
-    public static String solution(String new_id) {
-        new_id = new_id.toLowerCase();
-
-        new_id = new_id.replaceAll("[^a-z0-9-_.]", "");
-
-        new_id = new_id.replaceAll("\\.{2,}", ".");
-
-        new_id = new_id.replaceAll("^\\.|\\.$", "");
-
-
-        if (new_id.isEmpty()) {
-            new_id = "a";
-        }
-
-        if (new_id.length() > 15) {
-            new_id = new_id.substring(0, 15);
-            new_id = new_id.replaceAll("\\.$", "");
-        }
-
-        while (new_id.length() < 3) {
-            new_id += new_id.charAt(new_id.length() - 1);
-        }
-
-        return new_id;
-    }
-
-    public static void main(String[] args) {
-        String new_id = "...!@BaT#*..y.abcdefghijklm";
-        System.out.println(solution(new_id));
+    public String solution(String new_id) {
+        String answer = "";
+        return answer;
     }
 }
