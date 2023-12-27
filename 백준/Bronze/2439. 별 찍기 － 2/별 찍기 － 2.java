5ea89432615451dd0b0789e3be12e4f91f9cf31d
jodonghyeon3 <117457834+jodonghyeon3@users.noreply.github.com>
2023-12-27 23:38:52 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 344 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
